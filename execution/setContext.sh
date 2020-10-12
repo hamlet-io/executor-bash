a275558ccdd220c3296d78b47eb350b874795a58
Michael Foley <roleyfoley@gmail.com>
2020-10-12 22:57:39 +1100

    Fix cache creation (#110)
    * fix: cache location in setContext
    
    * fix: cache location for setcontext

--- execution/setContext.sh
+++ execution/setContext.sh
@@ -131,13 +131,17 @@ findGen3Dirs "${GENERATION_DATA_DIR}" || exit
 export GENERATION_INPUT_SOURCE="${GENERATION_INPUT_SOURCE:-"composite"}"
 
 if [[ "${GENERATION_INPUT_SOURCE}" == "composite" ]]; then
+
+    # Cache for asssembled components
+    export CACHE_DIR="$( getCacheDir "${GENERATION_CACHE_DIR}" )"
+
     blueprint_alternate_dirs=( \
     "${SEGMENT_SOLUTIONS_DIR}" \
     "${ENVIRONMENT_SHARED_SOLUTIONS_DIR}" \
     "${SEGMENT_SHARED_SOLUTIONS_DIR}" \
     "${PRODUCT_SHARED_SOLUTIONS_DIR}" )
 
-    export COMPOSITE_BLUEPRINT="${GENERATION_CACHE_DIR}/composite_blueprint.json"
+    export COMPOSITE_BLUEPRINT="${CACHE_DIR}/composite_blueprint.json"
     if [[ (("${GENERATION_USE_CACHE}" != "true") &&
             ("${GENERATION_USE_BLUEPRINT_CACHE}" != "true")) ||
         (! -f "${COMPOSITE_BLUEPRINT}") ]]; then
