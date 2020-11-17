7104af9c6517af2a5df985b443b52be2627c8dda
Michael Leditschke <mike@ammd.com.au>
2020-11-17 19:18:23 +1100

    refactor: remove cmdb tagging
    The tagging of the cmdb to reflect automation activities has not
    proven of use so this PR removes it from the various points where it
    was previously performed.
    
    Given that the same information as the tag is included in the cmdb
    commit message, the PR refactors it to be compliant with the
    conventional commit standard, which makes it more usable. To minimise
    the change needed to achieve this, the commit message provided to
    manageRepo is assumed to be name/value pairs and key named values used
    to construct the conventional commit. Two extra named parameters are
    included to allow explicit control over the type and description.

--- automation/jenkins/aws/acceptRelease.sh
+++ automation/jenkins/aws/acceptRelease.sh
@@ -14,19 +14,4 @@ RESULT=$? && [[ "${RESULT}" -ne 0 ]] && exit
 
 # Tag the builds
 ${AUTOMATION_DIR}/manageBuildReferences.sh -a "${RELEASE_TAG}"
-RESULT=$? && [[ "${RESULT}" -ne 0 ]] && exit
-
-# Verify the build information
-if [[ "${RELEASE_MODE}" == "${RELEASE_MODE_ACCEPTANCE}" ]]; then
-
-    # Record acceptance of the config
-    save_product_config "${DETAIL_MESSAGE}" "${PRODUCT_CONFIG_REFERENCE}" "${RELEASE_MODE_TAG}"
-    RESULT=$? && [[ ${RESULT} -ne 0 ]] && exit
-
-    # Record acceptance of the infrastructure
-    save_product_infrastructure "${DETAIL_MESSAGE}" "${PRODUCT_INFRASTRUCTURE_REFERENCE}" "${RELEASE_MODE_TAG}"
-    RESULT=$? && [[ ${RESULT} -ne 0 ]] && exit
-fi
-
-# All good
-RESULT=0
+RESULT=$?
--- automation/jenkins/aws/deploy.sh
+++ automation/jenkins/aws/deploy.sh
@@ -5,19 +5,15 @@ trap 'exit 1' SIGHUP SIGINT SIGTERM
 . "${AUTOMATION_BASE_DIR}/common.sh"
 
 function main() {
-    if [[ "${SEGMENT}" == "default" ]]; then
-        TAG="deploy${AUTOMATION_JOB_IDENTIFIER}-${PRODUCT}-${ENVIRONMENT}"
-    else
-        TAG="deploy${AUTOMATION_JOB_IDENTIFIER}-${PRODUCT}-${ENVIRONMENT}-${SEGMENT}"
-    fi
-    # Create the templates
-    ${AUTOMATION_DIR}/manageUnits.sh -l "application" -a "${DEPLOYMENT_UNIT_LIST}" -r "${PRODUCT_CONFIG_COMMIT}" || return $?
+  # Add conventional commit details
+  DETAIL_MESSAGE="${DETAIL_MESSAGE}, cctype=deploy, ccdesc=${AUTOMATION_JOB_IDENTIFIER}"
+  save_context_property DETAIL_MESSAGE
 
-    # All ok so tag the config repo
-    save_product_config "${DETAIL_MESSAGE}" "${PRODUCT_CONFIG_REFERENCE}" "${TAG}" || return $?
+  # Create the templates
+  ${AUTOMATION_DIR}/manageUnits.sh -l "application" -a "${DEPLOYMENT_UNIT_LIST}" -r "${PRODUCT_CONFIG_COMMIT}" || return $?
 
-    # Commit the generated application templates
-    save_product_infrastructure "${DETAIL_MESSAGE}" "${PRODUCT_INFRASTRUCTURE_REFERENCE}" "${TAG}" || return $?
+  # Commit the generated application templates
+  save_product_infrastructure "${DETAIL_MESSAGE}" "${PRODUCT_INFRASTRUCTURE_REFERENCE}" || return $?
 }
 
 main "$@"
